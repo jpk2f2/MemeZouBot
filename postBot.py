4bf776ee4f0b2c111e6d4cb74600ea329f33f447
sarahtwomey2 <31322452+sarahtwomey2@users.noreply.github.com>
2017-11-05 17:46:42 -0600

    Update postBot.py
    This update puts a link to an image in the post.

--- postBot.py
+++ postBot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#this bot posts text to a subreddit
+#this bot posts text to a subreddit, specifically a hard-coded link to an image
+#to connect this to the website we should change the title variable to whatever the user wants it to be
+#and change the structure varaible to a link to the meme the user created.
 
 import praw
 import pdb
@@ -8,6 +10,8 @@ import os
 import logging
 import subprocess
 import requests
+import webbrowser
+
 
 #Reddit instance
 reddit = praw.Reddit('bot1')
@@ -15,8 +19,8 @@ reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit('memezou')
 
 
-structure = "Hello World"
-message = "Hello World"
+title = "Hello World"
+structure = "https://i.pinimg.com/736x/05/91/fd/0591fdd8374b00d310d11d87c6150f88--excited-cat-beautiful-love.jpg"
 
-message2 = subreddit.submit(message, structure, send_replies=False)
+message = subreddit.submit(title, structure, send_replies=False)
 print("posting to r/memezou")
